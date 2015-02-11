c7da014a2e15374f4274d3ee59eebc500249ab3f
Théo Lassaube <theo-alexis@club-internet.fr>
2015-02-11 16:09:57 +0100

    config et dossiers

---
+++ include/texte.inc.php
@@ -0,0 +1,17 @@
+<div>
+<!--<?php
+/*if (!empty($_GET["page"])){
+	$page=$_GET["page"];}
+	else
+	{$page=0;
+	}
+switch ($page) {
+
+case 0:
+	include_once('pages/accueil.inc.php');
+	break;
+
+default : 	include_once('pages/accueil.inc.php');
+}*/
+?>-->
+</div>
