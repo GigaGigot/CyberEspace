33b8bd669496bd9b3e049cb78e6c73c4e74604be
BastienRebeyrat <bastien.rebeyrat@gmail.com>
2015-02-11 16:39:54 +0100

    texte

--- include/texte.inc.php
+++ include/texte.inc.php
@@ -1,17 +1,17 @@
 <div>
-<!--<?php
-/*if (!empty($_GET["page"])){
+<?php
+if (!empty($_GET["page"])){
 	$page=$_GET["page"];}
 	else
 	{$page=0;
 	}
 switch ($page) {
 
-case 0:
-	include_once('pages/accueil.inc.php');
+case 1:
+	include_once('pages/catalogue.inc.php');
 	break;
 
 default : 	include_once('pages/accueil.inc.php');
-}*/
-?>-->
+}
+?>
 </div>
