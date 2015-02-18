ddcca4f7c05566f1798d2e32ad3f78173060beb9
Arnaud Strill <arnaud.strill@gmail.com>
2015-02-18 08:29:23 +0100

    UTF8
    Modification du charset

--- include/header.inc.php
+++ include/header.inc.php
@@ -5,7 +5,8 @@
 
 <head>
 
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="content-type" content="text/html"/>
+    <meta charset="utf-8">
     <script type="text/javascript" src="js/fonctions.js"></script>
 
 	<?php $title=""; ?>
