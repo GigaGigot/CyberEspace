e6b100e33e56825c1d73f2e9110e101f52113e0e
Aundinn <C.S.V.X.7@live.fr>
2015-03-11 10:36:04 +0100

    fonction.js

--- js/fonctions.js
+++ js/fonctions.js
@@ -22,3 +22,34 @@ function option() {
     $(this).children(".solo").slideToggle();
     $(this).children(".multi").slideToggle();
 }
+
+function recupId(id){
+    num = id;
+    //alert(num);
+    setCookie('id',id);
+    alert(getCookie('id'));
+}
+
+function setCookie(sName, sValue) {
+        var today = new Date(), expires = new Date();
+        expires.setTime(today.getTime() + (365*24*60*60*1000));
+        document.cookie = sName + "=" + encodeURIComponent(sValue) + ";expires=" + expires.toGMTString();
+}
+
+function getCookie(sName) {
+    var cookContent = document.cookie, cookEnd, i, j;
+    var sName = sName + "=";
+    
+    for (i=0, c=cookContent.length; i<c; i++) {
+        j = i + sName.length;
+        if (cookContent.substring(i, j) == sName) {
+            cookEnd = cookContent.indexOf(";", j);
+            if (cookEnd == -1) {
+                cookEnd = cookContent.length;
+            }
+            return decodeURIComponent(cookContent.substring(j, cookEnd));
+        }
+    }       
+       
+    return "erreur";
+}
