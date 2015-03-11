c14100d1cc5b97742f7f5fa2f3b112b063bc70e1
BastienRebeyrat <bastien.rebeyrat@gmail.com>
2015-03-11 10:10:58 +0100

    tchat

--- include/pages/jeuMulti.inc.php
+++ include/pages/jeuMulti.inc.php
@@ -1,5 +1,77 @@
 <h1>Le coin des gens qui aiment jouer ensemble</h1>
 
 <div class="jeu"></div>
-<div class="chat"></div>
+    <div class="chat">
+        <div id="afficheMessage">
+        </div>
+        
+        <hr/>
+        <form>
+            Votre message : <br/><textarea id="saisieMessage" name="saisieMessage"></textarea></br>
+            <input id="btnTchat" value="Envoyer">
+        </form>
+    </div>
 <div class="boutik"></div>
+
+<script>
+    $(document).ready(function(){
+        
+        function charger(){
+            //e.preventDefault();
+            
+            setTimeout(
+                function() 
+                {
+                    //do something special
+                    var pseudo = encodeURIComponent( $('#pseudo').val());
+                    var message = encodeURIComponent( $('#message').val());
+
+            if(pseudo != "" && message != ""){
+                $.ajax({
+                    url : "include/pages/charger.php",
+                    type : "POST",
+                    data : "&message=" + message,
+                    success : function(data){
+                        $('#afficheMessage').html(data);
+                    },
+                    error : function(xhr,exception,message){
+                        alert(message);
+                    }
+                });
+            }
+                    charger();
+                }, 100);
+            
+            
+        }
+        function ecrire(){
+            //e.preventDefault();
+ 
+                    var pseudo = encodeURIComponent( $('#pseudo').val());
+                var message = encodeURIComponent( $('#message').val());
+
+            if(pseudo != "" && message != ""){
+                $.ajax({
+                    url : "include/pages/ecrire.php",
+                    type : "POST",
+                    data : "&message=" + $('#saisieMessage').val(),
+                    success : function(data){
+                        $('#afficheMessage').html(data);
+                    },
+                    error : function(xhr,exception,message){
+                        alert(message);
+                    }
+                });
+            }
+            $('#saisieMessage').val("");
+        }
+        
+        $('#btnTchat').click(function(e){
+            ecrire();
+        });
+        
+        $('#btnTchat').click();
+        charger();
+
+  });
+</script>
--- js/fonctions.js
+++ js/fonctions.js
@@ -1,6 +1,7 @@
 var estClique = false;
 
 $(document).ready(function() {
+charger();
                         $(".jeux").on('mouseover',grossissement);
                         $(".jeux").on('mouseout',reduction);
                         $(".jeux").on('click' ,option);
@@ -23,4 +24,40 @@ function option(){
     estClique = true;
     $(this).append("<input class='solo' type='sumbit' value='Solo' onclick=\"location.href='index.php?page=3'\">");
     $(this).append("<input class='multi' type='sumbit' value='Multi' onclick=\"location.href='index.php?page=4'\">")
+}
+
+/*$('#btnTchat').click(function(e){
+    e.preventDefault();
+
+    alert('coucou');
+    
+    var pseudo = encodeURIComponent( $('#pseudo').val());
+    var message = encodeURIComponent( $('#message').val());
+
+    if(pseudo != "" && message != ""){
+        $.ajax({
+            url : "../include/pages/charger.php",
+            type : "POST",
+            data : "&message=" + message,
+            success : function(data){
+                $('#afficheMessage').html(data);
+            },
+            error : function(xhr,exception,message){
+                alert(message);
+            }
+        });
+    }
+});*/
+
+function charger(){
+    setTimeout( function(){
+        $.ajax({
+            url : "index.php?page=4",
+            type : "GET",
+            success : function(html){
+                $('#messages').prepend(html);
+            }
+        });
+        charger();
+    }, 5000);
 }
