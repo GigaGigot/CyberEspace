b36f893465e9b1c7cfdc4ec0eb5063397e8d2388
Arnaud Strill <arnaud.strill@gmail.com>
2015-03-11 10:52:19 +0100

    Suppression fichier inutile

--- include/pages/tournoi.inc.php
+++ include/pages/tournoi.inc.php
@@ -19,38 +19,88 @@
         <tr>
             <td>Jeu</td>
             <td colspan="2">
-                <select name="jeuChoisi" onchange="NbJoueurs(this.value)">
-                    <option value="0">...</option>
-                    <option value="Bataille Navale">Bataille Navale</option>
-                    <option value="Président">Président</option>
-                    <option value="Belote">Belote</option>
-                    <option value="Monopoly">Monopoly</option>
+                <select name="jeuChoisi" onchange="NbJoueursMax(this.value)">
+                    <option value="0" selected="selected">...</option>
+                    <option value="1">Bataille Navale</option>
+                    <option value="2">Président</option>
+                    <option value="3">Belote</option>
+                    <option value="4">Monopoly</option>
                 </select>
             </td>
         </tr>
         <script>
-            function NbJoueurs(value) {
+            function NbJoueursMax(value) {
+                var nombre = document.getElementById('nbJoueurs');
+                while(nombre.hasChildNodes()){
+                   nombre.removeChild(nombre.childNodes[0]);
+                }
+                        
                 var joueurs = 0;
 
-                switch ($value) {
-                    case "Bataille Navale":
-                        $joueurs = 2;
+                switch (parseInt(value)) {
+                    case 1:
+                        joueurs = 2;
                         break;
 
-                    case "Président":
-                        $joueurs = 4;
+                    case 2:
+                        joueurs = 4;
                         break;
 
-                    case "Belote":
-                        $joueurs = 4;
+                    case 3:
+                        joueurs = 4;
                         break;
 
-                    case "Monopoly":
-
+                    case 4:
+                        joueurs = 4;
                         break;
+                        
+                    default:
+                        joueurs = 2;
+                        break;
+                }
+                        
+                var optionDepart = document.createElement("option");
+                optionDepart.setAttribute("value", 0);
+                optionDepart.setAttribute("selected", "selected");
+                optionDepart.setAttribute("id", (joueurs+'joueurs'));
+                optionDepart.appendChild(document.createTextNode("..."));
+                document.getElementById('nbJoueurs').appendChild(optionDepart);
+                        
+                while (joueurs > 1){
+                    var nouveauJoueur = document.createElement("option");
+                    nouveauJoueur.setAttribute("value", joueurs);
+                    nouveauJoueur.setAttribute("id", (joueurs+'joueurs'));
+                    var text = document.createTextNode(joueurs + ' joueurs');
+                    nouveauJoueur.appendChild(text);
+                    document.getElementById('nbJoueurs').appendChild(nouveauJoueur);
+                    joueurs = joueurs - 1;
                 }
             }
         </script>
+        
+        <tr>
+            <td>Nombre de joueurs</td>
+            <td colspan="2">
+                <select id="nbJoueurs" onchange="NbJoueursReels(this.value)">
+                    
+                </select>
+            </td>
+        </tr>
+        
+        <script>
+            function NbJoueursReels(value){
+                for(var i = 0; i < value; i++){
+                    var tr = document.createElement("tr");
+                    var label = document.createElement("td");
+                    label.appendChild(document.createTextNode("Joueur " + i));
+                    var td2 = document.createElement("td");
+                    var input = document.createElement("input"); // TODO
+                    input.setAttribute("id", ("joueur"+i));
+                }
+            }
+        </script>
+        
+        </div id="joueurs">
         <tr>
             <td>Joueur 1</td>
             <td colspan="2">
